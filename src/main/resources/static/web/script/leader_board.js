1ea8adc7e3f6908360fcdc5b0eb735ded5042994
OttaviaFeletig <ottafel@hotmail.it>
2018-12-19 12:00:31 +0100

    leader board done

--- src/main/resources/static/web/script/leader_board.js
+++ src/main/resources/static/web/script/leader_board.js
@@ -1,27 +1,69 @@
 var dataObj = new Vue({
-    
+
     el: '#leaderBoard',
     data: {
         urlGame: 'http://localhost:8080/api/leader_board',
         playersList: [],
-        isLoading: true        
+        won: 0,
+        lost: 0,
+        tied: 0,
+        isLoading: true
     },
-    created(){
-        this.loadFetchGame(this.urlGame)   
+    created() {
+        this.loadFetchGame(this.urlGame)
+
     },
     methods: {
-        loadFetchGame(url){
+        loadFetchGame(url) {
             fetch(url, {
-                method: 'GET',
-            })
+                    method: 'GET',
+                })
                 .then(response => response.json())
                 .then(data => {
-                    this.isLoading = false;
                     this.playersList = data;
-                    console.log(this.playersList);    
-            })
+                    console.log(this.playersList);
+                    this.calculateTotalScore();
+                    console.log(this.playersList);
+                    this.calculateResults();
+                    console.log(this.playersList);
+                    this.isLoading = false;
+                })
+        },
+        calculateTotalScore() {
+            for (var i = 0; i < this.playersList.length; i++) {
+                this.playersList[i]["total_score"] =
+                    this.playersList[i].scores
+                    .reduce((a, b) => a + b, 0) || "N/C";
+            }
+        },
+        calculateResults() {
+            for (var i = 0; i < this.playersList.length; i++) {
+                let won = 0;
+                let tied = 0;
+                let lost = 0;
+                if (this.playersList[i].scores.length > 0) {
+                    this.playersList[i].scores.map(score => {
+                        if (score < 0.5) {
+                            lost++
+                        } else if (score == 0.5) {
+                            tied++
+                        } else {
+                            won++
+                        }
+                        this.playersList[i]["won"] = won;
+                        this.playersList[i]["tied"] = tied;
+                        this.playersList[i]["lost"] = lost;
+                    })
+                }else{
+                    this.playersList[i]["won"] = "-";
+                    this.playersList[i]["tied"] = "-";
+                    this.playersList[i]["lost"] = "-";
+                }
+
+                //                console.log(wins)
+            }
         }
-    }
-    
-    
+    },
+
+
 })
