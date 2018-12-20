ae6c83dc563b0e9996ccae684f09d21257742e49
OttaviaFeletig <ottafel@hotmail.it>
2018-12-20 13:07:32 +0100

    leaderboard and games list joined

--- src/main/resources/static/web/script/games.js
+++ src/main/resources/static/web/script/games.js
@@ -1,32 +1,69 @@
 var dataObj = new Vue({
-    
-    el: '#gameList',
+
+    el: '#games',
     data: {
-        urlGame: 'http://localhost:8080/api/games',
+        urlArray: ['http://localhost:8080/api/games', 'http://localhost:8080/api/leader_board'],
         gameList: [],
         date: [],
-        isLoading: true        
+        playersList: [],
+        isLoading: true
     },
-    created(){
-        this.loadFetchGame(this.urlGame)   
+    created() {
+        this.loadFetchGame(this.urlArray)
     },
     methods: {
-        loadFetchGame(url){
-            fetch(url, {
-                method: 'GET',
-            })
-                .then(response => response.json())
-                .then(data => {
-                    this.isLoading = false;
-                    this.gameList = data;
-                    console.log(this.gameList);
+        loadFetchGame(urlArray) {
+            Promise.all(urlArray
+                    .map(url => fetch(url)
+                        .then(response => response.json())))
+                .then(values => {
+                    this.gameList = values[0];
+                    this.playersList = values[1];
+
+                    console.log(this.gameList)
+                    console.log(this.playersList)
                     this.convertDate();
-            })
+                    this.calculateTotalScore();
+                    this.calculateResults();
+                    this.isLoading = false;
+                })
         },
-        convertDate(){    
+        convertDate() {
             this.gameList.map(game => game.created = new Date(game.created).toLocaleString())
+        },
+        calculateTotalScore() {
+            for (var i = 0; i < this.playersList.length; i++) {
+                this.playersList[i]["total_score"] =
+                    this.playersList[i].scores
+                    .reduce((a, b) => a + b, 0) || "N/C";
+            }
+        },
+        calculateResults() {
+            for (var i = 0; i < this.playersList.length; i++) {
+                let won = 0;
+                let tied = 0;
+                let lost = 0;
+                if (this.playersList[i].scores.length > 0) {
+                    this.playersList[i].scores.map(score => {
+                        if (score < 0.5) {
+                            lost++
+                        } else if (score == 0.5) {
+                            tied++
+                        } else {
+                            won++
+                        }
+                        this.playersList[i]["won"] = won;
+                        this.playersList[i]["tied"] = tied;
+                        this.playersList[i]["lost"] = lost;
+                    })
+                } else {
+                    this.playersList[i]["won"] = "-";
+                    this.playersList[i]["tied"] = "-";
+                    this.playersList[i]["lost"] = "-";
+                }
+            }
         }
     }
-    
-    
+
+
 })
