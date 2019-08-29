8494e69256f65713242b7badc2562866ef80e5fe
OttaviaFeletig <ottafel@hotmail.it>
2019-08-29 11:54:29 +0200

    repo cleaned

--- src/main/java/com/codeoftheweb/salvo/ScoreRepository.java
+++ src/main/java/com/codeoftheweb/salvo/ScoreRepository.java
@@ -1,10 +1,8 @@
 package com.codeoftheweb.salvo;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ScoreRepository extends JpaRepository<Score, Long> {
-//    List<Player> findByUserName(String userName);
 }
--- src/main/java/com/codeoftheweb/salvo/ShipRepository.java
+++ src/main/java/com/codeoftheweb/salvo/ShipRepository.java
@@ -1,10 +1,8 @@
 package com.codeoftheweb.salvo;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ShipRepository extends JpaRepository<Ship, Long> {
-//    List<Player> findByUserName(String userName);
 }
